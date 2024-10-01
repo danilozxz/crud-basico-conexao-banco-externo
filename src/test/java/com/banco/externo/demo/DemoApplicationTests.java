26255795bea40f32f0c7448d50fa6ee7b89a9a73
Danilozxz <progdanilo@gmail.com>
2024-10-01 16:39:01 -0300

    Commit inicial para a criação do projeto Spring Boot

---
+++ src/test/java/com/banco/externo/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.banco.externo.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
